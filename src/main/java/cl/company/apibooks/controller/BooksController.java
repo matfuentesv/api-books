c827b32efb4b3bd22b8e58bc01e6ea08e27414f0
O009920 <mfuentes@forum.cl>
2024-10-16 22:09:14 -0300

    subida inicial api

---
+++ src/main/java/cl/company/apibooks/controller/BooksController.java
@@ -0,0 +1,22 @@
+package cl.company.apibooks.controller;
+
+import lombok.extern.java.Log;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("/api")
+@Log
+public class BooksController {
+
+    @GetMapping(value = "/books", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getAllMovies() {
+        log.info("Se solicita la lista de todas las películas.");
+        return ResponseEntity.ok("Hola Mundo");
+    }
+}
---
+++ src/test/java/cl/company/apibooks/ApiBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.company.apibooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBooksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
